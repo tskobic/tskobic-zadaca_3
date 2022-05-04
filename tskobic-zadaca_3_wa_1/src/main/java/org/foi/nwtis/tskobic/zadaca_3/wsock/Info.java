f9efe824263ce2790cce9bf1c51848c3e909d007
tskobic <tskobic@58350e7a-5921-49bc-b2bb-43b6036b2832>
2022-05-04 06:58:04 +0000

    4.5. rad na labosima nedovrseno

--- tskobic-zadaca_3_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_3/wsock/Info.java
+++ tskobic-zadaca_3_wa_1/src/main/java/org/foi/nwtis/tskobic/zadaca_3/wsock/Info.java
@@ -2,7 +2,6 @@ package org.foi.nwtis.tskobic.zadaca_3.wsock;
 
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 import jakarta.websocket.CloseReason;
@@ -19,36 +18,36 @@ public class Info {
 	Set<Session> sesije = new HashSet<>();
 	
 	public void posaljiPoruku(String poruka) {
-		for (Session sesija : sesije) {
+		for(Session sesija: sesije) {
 			if(sesija.isOpen()) {
 				try {
 					sesija.getBasicRemote().sendText(poruka);
 				} catch (IOException e) {
 					System.out.println("Pogreška kod slanja poruke za sesiju: " + sesija.getId());
 				}
-			} 
+			}
 		}
 	}
-
+	
 	@OnOpen
 	public void otvori(Session sesija, EndpointConfig konfig) {
 		sesije.add(sesija);
-		System.out.println("Otvorena veza : " + sesija.getId());
+		System.out.println("Otvorena veza: " + sesija.getId());
 	}
 
 	@OnClose
 	public void zatvori(Session sesija, CloseReason razlog) {
-		System.out.println("Zatvorena veza : " + sesija.getId() + " Razlog: " + razlog.getReasonPhrase());
+		System.out.println("Zatvorena veza: " + sesija.getId() + " Razlog: " + razlog.getReasonPhrase());
 		sesije.remove(sesija);
 	}
-	
+
 	@OnMessage
 	public void stiglaPoruka(Session sesija, String poruka) {
-		System.out.println("Veza : " + sesija.getId() + " Poruka: " + poruka);
+		System.out.println("Veza: " + sesija.getId() + " Poruka: " + poruka);
 	}
-	
+
 	@OnError
 	public void pogreska(Session sesija, Throwable iznimka) {
-		System.out.println("Veza : " + sesija.getId() + " Pogreška: " + iznimka.getMessage());
+		System.out.println("Veza: " + sesija.getId() + " Pogreška: " + iznimka.getMessage());
 	}
 }
